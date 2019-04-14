df786842eded4f1bf062bd9f6b7ee0c15cc9e02c
Artsiom <artem.ice33@gmail.com>
2019-04-14 20:13:20 +0300

    task 03 completed

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,9 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   let year = date.getFullYear();
+   if(((year % 400=== 0) || (year % 100 !== 0)) && (year % 4 === 0)) return true;
+   return false;
 }
 
 
@@ -76,7 +78,10 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   let start = new Date(startDate);
+   let end = new Date(endDate);
+   let elapsed = new Date(end - start).toISOString();
+   return elapsed.substring(11, elapsed.length - 1);
 }
 
 
@@ -94,7 +99,11 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+   let hours = date.getUTCHours();
+   let minutes = date.getUTCMinutes();
+   let angle = Math.abs((hours%12)*60 + minutes - minutes*12)*Math.PI/360;
+   if(angle > Math.PI) return angle - Math.PI
+   return angle;
 }
 
 
